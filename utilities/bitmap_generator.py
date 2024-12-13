a5bf8c8921525c5488eb47670ffdc3dca2b3fd3b
saurav <sauravsajeev202@gmail.com>
2024-12-14 02:17:31 +0530

    Fixed issues causing images with height not a factor of 8 to not adjust

--- utilities/bitmap_generator.py
+++ utilities/bitmap_generator.py
@@ -1,21 +1,31 @@
 import sys
 from PIL import Image
 
+w = 0
+h = 0
+
+
 def image_to_bitmap(image_path, threshold=50):
     image = Image.open(image_path)
     grayscale_image = image.convert("L")
     bw_image = grayscale_image.point(lambda x: 0 if x < threshold else 1, "1")
     width, height = bw_image.size
     bitmap_array = []
-    bitmap_array.append("const uint8_t PROGMEM splash[] = {")
+    global w, h
+    w = (width + 7) & ~7
+    h = (height + 7) & ~7
+    bitmap_array.append(f"const uint8_t PROGMEM splash[{w}*{h}] = " + "{")
     c = 0
-    for y in range(height):
+    for y in range(h):
         row = []
         row.append("0b")
         for x in range(width):
             c += 1
-            pixel = bw_image.getpixel((x, y))
-            row.append(str(pixel))
+            if y < height:
+                pixel = bw_image.getpixel((x, y))
+                row.append(str(pixel))
+            else:
+                row.append("0")
             if x > 0:
                 if c == width and c % 8 != 0:
                     for p in range(((8 - (width % 8)) + width) - c):
@@ -46,7 +56,7 @@ if __name__ == "__main__":
         file.write("\n".join(bitmap))
 
     print(
-        f"Bitmap written to {output_file}. Do you want to print it here?\nPress [Y] to print."
+        f"Bitmap written to {output_file}.\nBitmap Size: {w}x{h} pixels\nDo you want to print it here?\nPress [Y] to print."
     )
     if len(sys.argv) == 2:
         if sys.argv[1] == "Y".lower():
