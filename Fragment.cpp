25618d7924c3f66c6d14534d7da05e1593e73f71
saurav <sauravsajeev202@gmail.com>
2024-12-14 02:19:02 +0530

    Fragment implementation

---
+++ Fragment.cpp
@@ -0,0 +1,52 @@
+#include "SSD1306.h"
+
+/**
+ * @author Saurav Sajeev
+ */
+
+FragmentManager::FragmentManager(OLED &oled) : oled(&oled) {}
+
+OLED *FragmentManager::getOLED()
+{
+    return oled;
+}
+
+Fragment::Fragment(FragmentManager &manager)
+    : manager(manager), drawableCount(0), drawableCapacity(2)
+{
+    drawables = new Drawable *[drawableCapacity];
+}
+
+Fragment::~Fragment()
+{
+    for (int i = 0; i < drawableCount; ++i)
+    {
+        delete drawables[i];
+    }
+    delete[] drawables;
+}
+
+void Fragment::add(Drawable *drawable)
+{
+    if (drawableCount >= drawableCapacity)
+    {
+        drawableCapacity *= 2;
+        Drawable **newDrawables = new Drawable *[drawableCapacity];
+        for (int i = 0; i < drawableCount; ++i)
+        {
+            newDrawables[i] = drawables[i];
+        }
+        delete[] drawables;
+        drawables = newDrawables;
+    }
+    drawables[drawableCount++] = drawable;
+}
+
+void Fragment::inflate()
+{
+    OLED &oled = *manager.getOLED();
+    for (int i = 0; i < drawableCount; ++i)
+    {
+        drawables[i]->draw(oled);
+    }
+}
