d1c3f0d73ce12455d438bc4b646c9fcf01e41803
saurav <sauravsajeev202@gmail.com>
2025-02-09 00:39:19 +0530

    Add HelloWorld example and update Shapes example to include inflate calls for rendering

--- examples/Shapes/Shapes.ino
+++ examples/Shapes/Shapes.ino
@@ -12,16 +12,19 @@ void setup() {
   
   // Draw a rectangle at (10, 10) with width 50, height 30, 5px corner radius, and 2px thickness
   oled.rectangle(10, 10, 50, 30, 5, 2, false);
+  oled.inflate(); //  Render the items on the display
 
   delay(1000);
   
   // Draw a circle at (64, 32) with radius 20, and thickness 2px
   oled.circle(64, 32, 20, 2);
+  oled.inflate(); //  Render the items on the display
 
   delay(1000);
   
   // Draw a line from (0, 0) to (127, 63), 2px thick
   oled.line(0, 0, 127, 63, 2);
+  oled.inflate(); //  Render the items on the display
 }
 
 void loop() {
